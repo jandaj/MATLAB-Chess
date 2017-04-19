5a2eac953768356ab332dae2e1c78056436914d5
Mohit Khattar <mok3112@gmail.com>
2017-04-19 15:00:08 -0400

    Move function to move strings around the board

---
+++ ChessGame/moveToSpace.m
@@ -0,0 +1,22 @@
+% This function works for any GUI object, but it requires a global field of
+% handles to be declared, called pieceString. If you want to use it with
+% some other data type, you'll have to use different
+% comparison/getter/setter methods to make the function work.
+
+function handles = moveToSpace(buttonObj, handles)    
+    if strcmp(handles.pieceString, '') == 1 && ...
+            strcmp(get(buttonObj, 'String'), '') == 0
+        handles.pieceString = get(buttonObj, 'String');
+        set(buttonObj, 'BackgroundColor', 'yellow');
+        set(buttonObj, 'String', '');
+        disp('Piece string is empty');
+        disp(handles.pieceString);
+    else
+        set(buttonObj, 'String', handles.pieceString);
+        h = findobj('BackgroundColor', 'yellow');
+        set(h, 'BackgroundColor', 'white');
+        handles.pieceString = '';
+        disp('Piece string is not empty');    
+    end
+    guidata(buttonObj, handles);    
+end
